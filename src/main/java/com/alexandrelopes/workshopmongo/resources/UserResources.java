3f81dab4a1b36da346f2625636f6b12dafb756a8
Alexandre Lopes <alexandre_lopess@hotmail.com>
2022-07-22 11:57:51 -0300

    Obtendo um usuário por id

--- src/main/java/com/alexandrelopes/workshopmongo/resources/UserResources.java
+++ src/main/java/com/alexandrelopes/workshopmongo/resources/UserResources.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,9 +25,14 @@ public class UserResources {
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
 		//Agora que estamos usando o UserDTO teremos que passar a lista de Users para uma stream e usar o metodo map para instanciar um novo userDTO a cada use na lista
-		
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) /* Também seria possível trocar esta annotation por @GetMapping que daria no mesmo */
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 
 }
