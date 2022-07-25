da16f5183d22f8f378200003393774c2c49a66fe
AlexandreGLopes <alexandre_lopess@hotmail.com>
2022-07-25 15:53:00 -0300

    Consulta com vários critérios

--- src/main/java/com/alexandrelopes/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alexandrelopes/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.alexandrelopes.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -24,5 +25,9 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	//referece: https://docs.mongodb.com/manual/reference/operator/query/regex/
 	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
 	List<Post> searchTitle(String text);
+	
+	//Search with many criteria
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte: ?2} }, { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 	
 }
