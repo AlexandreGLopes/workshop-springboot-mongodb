73b28b1125cfe6b66aef002d9cac0d45be16b2b8
AlexandreGLopes <alexandre_lopess@hotmail.com>
2022-07-25 15:13:30 -0300

    Consulta simples com @Query

--- src/main/java/com/alexandrelopes/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alexandrelopes/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.alexandrelopes.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alexandrelopes.workshopmongo.domain.Post;
@@ -19,5 +20,9 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	//The methods used in tgis project will be listed below:
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
+	//There is another way to do the same search, but with the @Query annotation
+	//referece: https://docs.mongodb.com/manual/reference/operator/query/regex/
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 }
